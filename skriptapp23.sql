ce31469eeca0dc36713c0b0e0c2d0bbcc4f61eea
Vedran Alagić <rvukelic@gmail.com>
2021-05-12 20:12:11 +0200

    Kreiranje tablica

---
+++ skriptapp23.sql
@@ -0,0 +1,14 @@
+drop database if exists firmapp23;
+create database firmapp23;
+use firmapp23;
+
+create table projekt;
+sifra int not null primary key auto_increment;
+naziv varchar (50);
+cijena decimal (18,2)
+);
+create table programer;
+sifra int not null primary key auto_increment;
+ime varchar (50)
+prezime int
+datumrodjenja dattime
